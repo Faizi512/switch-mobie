cf8d590263840970e78559414950a81002ef37c2
umar482 <umar.omtomo@gmail.com>
2020-01-30 08:32:41 +0000

    push new variable (#4)
    * indenting of code and add new variable in form
    
    * fix the save energy bill page js

--- app/javascript/packs/saveEnergy.js
+++ app/javascript/packs/saveEnergy.js
@@ -1,99 +1,69 @@
 $(document).on('turbolinks:load',function(){
   validate()
-  $(function(){
-    $('.name').text(getUrlParameter('firstname'))
-    $('.postcode').text(getUrlParameter('postcode'))
-     $("#submitbtn").click(function(event){
-      if ($('#engeryForm').parsley().validate()){
-        $('#submitbtn').prop('disabled', true);
-        postData()
-      }
-     });
-   })
-
-formValidation = {}
-var ip_Address = '';
-
+  formValidation = {}
+  var ip_Address = '';
+  $('.name').text(getUrlParameter('firstname'))
+  $('.postcode').text(getUrlParameter('postcode'))
+  $("#submitbtn").click(function(event){
+    if ($('#engeryForm').parsley().validate()){
+      $('#submitbtn').prop('disabled', true);
+      postData()
+    }
+  });
+  
   $.getJSON('https://ipapi.co/json/', function(data) {
     if (data != null && data.ip != undefined && typeof (data.ip) == "string") {
       ip_Address = data.ip;
     }
-  });
-function validate(){
-  formValidation = $('#engeryForm').parsley({
-        trigger: "focusout",
-        errorClass: 'error',
-        successClass: 'valid',
-        errorsWrapper: '<div class="parsley-error-list"></div>',
-        errorTemplate: '<label class="error"></label>',
-        errorsContainer (field) {
-          if(field.$element.hasClass('approve')){
-            return $('.error-checkbox')
-          }
-          return field.$element.parent()
-        },
-     })
-}
-//     // jQuery.validator.addMethod("ukpostcode", function(value, element) {
-//     //   return this.optional(element) || /([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9][A-Za-z]?))))\s?[0-9][A-Za-z]{2})/i.test(value);
-//     // }, "Invalid Uk postcode");
+  });  
+});
+  var ip_Address = '';
+  function validate(){
+    formValidation = $('#engeryForm').parsley({
+      trigger: "focusout",
+      errorClass: 'error',
+      successClass: 'valid',
+      errorsWrapper: '<div class="parsley-error-list"></div>',
+      errorTemplate: '<label class="error"></label>',
+      errorsContainer (field) {
+        if(field.$element.hasClass('approve')){
+          return $('.error-checkbox')
+        }
+        return field.$element.parent()
+      },
+    })
+  }
 
-//     return $( "#engeryForm" ).validate({
-//       ignore: ":hidden",
-//       rules: {
-//         address: {
-//           required: true,
-//         },
-
-//       },
-//       messages: {
-//         address:{
-//           required: 'Please Enter Your Address',
-//         },
- 
-//       },
-//       errorPlacement: function (error, element) {
-//         if (element.hasClass('radio-field')){
-//           $('.radio-error').append(error)
-//         }else{
-//           element.after(error)
-//         }
-//       }
-//     });
-   
-// }
-
-function getData() {
+  function getData() {
     var e = JSON.parse(localStorage.getItem("parameters"))
     var data = {
-          postcode: getUrlParameter('postcode')|| 'unknown',
-          firstname: getUrlParameter('firstname')|| 'unknown',
-          lastname: getUrlParameter('lastname')|| 'unknown',
-          email: getUrlParameter('email')|| 'unknown',
-          phone1: getUrlParameter('phone1')|| 'unknown',
-          street1: $(".address-field").val(),
-          lead_id: getUrlParameter('lead_id')|| '',
-          sid: getUrlParameter('sid')|| 1,
-          source: getUrlParameter('source') || '',
-          ssid: getUrlParameter('ssid') || 'unknown',
-          paymentmethod: getUrlParameter('paymentmethod')|| 'prepayment',
-          currentprovider: getUrlParameter('currentprovider')|| 'other',
-          prize: getUrlParameter('prize')|| 'mobilephone',
-          trafficid: getUrlParameter('trafficid')|| 'Save Energy Bill',
-          towncity: getUrlParameter('towncity')|| 'unknown',
-          title: getUrlParameter('title')|| 'Mr',
-          optindate: getFormattedCurrentDate(),
-          optinurl: 'http://deals.megamobiledeals.com/save_energy_bill',
-          ipaddress: ip_Address,
-          timestamp: new Date,
-          user_agent: window.navigator.userAgent,
-          parameters: e
-      };
-      localStorage.setItem("data", JSON.stringify(data))
+      postcode: getUrlParameter('postcode')|| 'unknown',
+      firstname: getUrlParameter('firstname')|| 'unknown',
+      lastname: getUrlParameter('lastname')|| 'unknown',
+      email: getUrlParameter('email')|| 'unknown',
+      phone1: getUrlParameter('phone1')|| 'unknown',
+      street1: $(".address-field").val(),
+      lead_id: getUrlParameter('lead_id')|| '',
+      sid: getUrlParameter('sid')|| 1,
+      source: getUrlParameter('source') || '',
+      ssid: getUrlParameter('ssid') || 'unknown',
+      paymentmethod: getUrlParameter('paymentmethod')|| 'prepayment',
+      currentprovider: getUrlParameter('currentprovider')|| 'other',
+      prize: getUrlParameter('prize')|| 'mobilephone',
+      trafficid: getUrlParameter('trafficid')|| 'Save Energy Bill',
+      towncity: getUrlParameter('towncity')|| 'unknown',
+      title: getUrlParameter('title')|| 'Mr',
+      optindate: getFormattedCurrentDate(),
+      optinurl: 'http://deals.megamobiledeals.com/save_energy_bill',
+      ipaddress: ip_Address,
+      timestamp: new Date,
+      user_agent: window.navigator.userAgent,
+      parameters: e
+    };
+    localStorage.setItem("data", JSON.stringify(data))
+  }
 
-}
-
-function postData() {
+  function postData() {
     getData();
     var e = JSON.parse(localStorage.getItem("data"));
     console.log(e)
@@ -108,42 +78,35 @@ function postData() {
       error: function(res) {
           console.log(res)
       },
-
     })
-}
+  }
 
-function getUrlParameter(sParam) {
+  function getUrlParameter(sParam) {
     var sPageURL = window.location.search.substring(1),
         sURLVariables = sPageURL.split('&'),
         sParameterName,
         i;
-
     for (i = 0; i < sURLVariables.length; i++) {
-        sParameterName = sURLVariables[i].split('=');
-
-        if (sParameterName[0] === sParam) {
-            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
-        }
+      sParameterName = sURLVariables[i].split('=');
+      if (sParameterName[0] === sParam) {
+        return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+      }
     }
-}
+  }
 
- function getFormattedCurrentDate() {
+  function getFormattedCurrentDate() {
     var date = new Date();
-
     var day = addZero(date.getDate());
     var monthIndex = addZero(date.getMonth() + 1);
     var year = date.getFullYear();
     var min = addZero(date.getMinutes());
     var hr = addZero(date.getHours());
     var ss = addZero(date.getSeconds());
-
     return day + '/' + monthIndex + '/' + year + ' ' + hr + ':' + min + ':' + ss;
   }
-
   function addZero(i) {
     if (i < 10) {
       i = "0" + i;
     }
     return i;
   }
-});
