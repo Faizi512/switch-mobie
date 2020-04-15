7ec551eb4ffb16609f698f7867a663ed721d35b5
umar482 <umar.omtomo@gmail.com>
2020-04-15 09:35:39 +0000

    Sim deals only amp page added (#99)
    * Sim deals only amp page added
    
    * Sim deals only amp page updated

--- app/controllers/amp_controller.rb
+++ app/controllers/amp_controller.rb
@@ -5,4 +5,9 @@ class AmpController < ApplicationController
 
   end
 
+  def sim_only_deals
+    render :layout => false
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 	  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 	get '/amp' => 'amp#index'
+  get '/amp/sim-only-deals' => 'amp#sim_only_deals'
   get '/:page_name' => 'pages#show'
   resources :facebook_custom_audience
 	root 'pages#index'
